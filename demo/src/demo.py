8ec54087dca13cb3335526706570ed0c8f614ef2
ThetisEliza <wxf199601@gmail.com>
2024-03-15 19:48:13 +0800

    feat: back-end mock data

--- demo/src/demo.py
+++ demo/src/demo.py
@@ -1,10 +1,38 @@
 from flask import Flask
+import sys
+from model.data import Group
+import json
+
+
+class System:
+    def __init__(self) -> None:
+        self.groups = []
+
+    def load_sys(self) -> 'System':
+        with open('/home/phoenix/projects/nuc-se-project/demo/tmp/groups.jsonl') as f:
+            for line in f:
+                self.groups.append(Group.parse(line.strip()))
+        return self
+
 
 app = Flask(__name__)
 
+system = System().load_sys()
+    
+    
 @app.route('/')
 def index():
     return 'welcome to my webpage!'
 
+@app.route('/groups/all')
+def get_all_groups():
+    return json.dumps(list(map(lambda x: x.__dict__, system.groups)), ensure_ascii=False)
+
+@app.route('/groups/page/<int:pageId>')
+def get_page_groups(pageId):
+    return json.dumps(list(map(lambda x: x.__dict__, system.groups))[5*pageId:5*(pageId+1)], ensure_ascii=False)
+
+
 if __name__ == '__main__':
+    
     app.run(port=8080, host='172.24.43.114', debug=True)
---
+++ demo/src/model/data.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+import json
+import random
+import re
+
+Names = """（1）Eric——艾利克　　Ron——罗恩
+（2）Lydia——莉迪娅　　Page——裴吉
+（3）Darcy——达茜　　Susan——苏珊
+（4）Ida——艾达　　Sally——莎莉
+（5）Una——优娜　　Ella——艾拉
+（6）Liz——莉兹　　Ray——拉伊
+（7）Nat——纳特　　Otis——奥狄斯
+（8）Phil——菲尔　　Reg——雷哲
+（9）Sam——山姆　　Alma——爱玛
+（10）Ira——艾勒　　Gina——吉娜
+（11）Alice——爱丽丝　　Ann——安妮
+（12）Bella——贝拉　　Cara——卡拉
+（13）Dora——多拉　　Hedy——赫蒂
+
+英文起名 80个好听又短的英文名字介绍
+（14）Iy——艾唯　　Jack——杰克
+（15）John——约翰　　Joab——乔布
+（16）Bob——鲍伯　　Dick——狄克
+（17）Eli——伊莱　　Ken——肯恩
+（18）Otto——奥特　　Alan——艾伦
+（19）Rita——莉达　　Sara——莎拉
+（20）Tess——泰丝　　Vera——维拉
+（21）Xenia——芝妮雅　　Cara——卡拉
+（22）Dana——黛娜　　Elva——艾娃
+（23）Bill——比尔　　Bert——伯特
+（24）Ben——班　　Carl——卡尔
+（25）Drew——杜鲁　　Earl——额尔
+（26）Gill——姬儿　　leo——利欧
+
+英文起名 80个好听又短的英文名字介绍
+（27）Jane——珍　　June——朱恩
+（28）Linda——琳达　　Lucy——露西
+（29）Guy——盖　　Hale——霍尔
+（30）Ian——伊恩　　Jo——乔
+（31）Will——维尔　　Les——勒斯
+（32）Anna——安娜　　Bess——贝丝
+（33）Dora——多拉　　Erin——艾琳
+（34）Eve——伊芙　　Fay——费怡
+（35）Don——多恩　　Eric——艾利克
+（36）Neil——尼尔　　Bard——巴德
+（37）Kim——金姆　　Jack——杰克
+（38）Len——伦恩　　Max——马克斯
+（39）Nina——妮娜　　Page——佩格
+（40）Zona——若娜　　Jon——琼恩"""
+
+NAMES = re.findall("[a-zA-Z]+?——\w+?", Names)
+
+
+@dataclass
+class Group:
+    name: str = ""
+    score: float = 0
+    
+    def to_json(self) -> str:
+        return json.dumps(self.__dict__, ensure_ascii=False)
+    
+    @staticmethod
+    def random_gen() -> 'Group':
+        return Group(random.choice(NAMES), random.randrange(0, 300, 10))
+    
+    def parse(data: str) -> 'Group':
+        return Group(**json.loads(data))
+    
+if __name__ == '__main__':
+    print(Group.random_gen())
