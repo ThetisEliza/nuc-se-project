f18e31527729e46bac78e66905cb65463edcab3d
ThetisEliza <wxf199601@gmail.com>
2024-03-19 08:39:49 +0800

    feat: fix details

--- nuc-se-front/src/views/AdminEdit.vue
+++ nuc-se-front/src/views/AdminEdit.vue
@@ -1,27 +1,49 @@
 <template>
     <div>
-        <v-data-table-server
-            v-model:items-per-page="groupsPerPage"
-            :items="groups"
-            :items-length="totalGroups"
-            :loading="loading"
-            :headers="headers"
-            item-value="name"
-            @update:options="loadItems"
-        >
+        <v-card flat>
+            <v-card-title class="d-flex align-center pe-2">
+                <v-icon icon="mdi-account-multiple"></v-icon> &nbsp;
+                团队
 
-            <template v-slot:item.members="{ item }">
-                <v-chip v-for="member in item.members" color="primary">{{ member.name }}</v-chip>
-            </template>
+                <v-spacer></v-spacer>
+                <v-text-field
+                    v-model="search"
+                    density="compact"
+                    label="查找团队"
+                    prepend-inner-icon="mdi-magnify"
+                    variant="solo-filled"
+                    flat
+                    hide-details
+                    single-line
+                ></v-text-field>
+            </v-card-title> 
+            <v-divider></v-divider>
+            <v-data-table-server
+                v-model:items-per-page="itemsPerPage"
+                :items="groups"
+                :items-length="totalGroups"
+                :loading="loading"
+                :headers="headers"
+                item-value="name"
+                items-per-page-text="每页"
+                @update:options="loadItems"
+            >
 
-            <template v-slot:item.edit="{ item }">
-                <v-btn v-bind="props" color="red"
-                @click="dialogOpen(item)"
-                >编辑
-                </v-btn>
-            </template>
-      
-        </v-data-table-server>
+                <template v-slot:item.members="{ item }">
+                    <v-chip v-for="member in item.members" color="primary">{{ member.name }}</v-chip>
+                </template>
+
+                <template v-slot:item.edit="{ item }">
+                    <v-btn v-bind="props" color="red"
+                    @click="dialogOpen(item)"
+                    >编辑
+                    </v-btn>
+                </template>
+        
+            </v-data-table-server>
+
+        </v-card>
+       
 
         <v-dialog
         v-model="editing"
@@ -37,12 +59,14 @@
                             cols="12"
                             md="4"
                             sm="6"
-            >
+                        >
                         <v-text-field
                             variant="outlined"
-                            v-model="editingScore"
-                            label="score"
+                            v-model="editingAddScore"
+                            label="添加分数"
                         ></v-text-field>
+
+                        <text>分数预览：{{ parseInt(editingGroup.score) + parseInt(editingAddScore) }}</text>
                         </v-col>
                     </v-row>
                     
@@ -51,7 +75,7 @@
                 <v-divider></v-divider>
 
                 
-                <v-card-actions>
+                <!-- <v-card-actions>
                     <v-chip-group>                     
                         <v-flex>
                             <v-chip 
@@ -72,7 +96,7 @@
                     <v-btn>
                         解散团队
                     </v-btn>
-                </v-card-actions>
+                </v-card-actions> -->
 
 
                 <v-divider></v-divider>
@@ -87,6 +111,7 @@
                     确认
                     </v-btn>
                     <v-btn
+                    color="green"
                     @click="dialogCancel()"
                     >
                     取消
@@ -124,8 +149,9 @@ export default defineComponent({
         loading: true,
 
         editing: false,
-        editingScore: 0,
-        editingGroup: null,        
+        editingAddScore: 0,
+        editingGroup: null,    
+        search: "",    
     }),
 
     mounted() {
@@ -134,14 +160,22 @@ export default defineComponent({
 
     methods: {
 
-        loadItems() {
-            groupService.getAllGroups()
+        loadItems({ page, itemsPerPage, sortBy, search }) {
+            groupService.getPageGroups({pageNum: page, groupsPerPage: itemsPerPage, sortBy: sortBy})
                 .then(res=>{
-                    this.$data.groups = res.data.groups
-                    this.$data.groups.forEach(group => {
+                    res.data.groups.forEach(group => {
                         group.regularScore = parseInt(group.regularScore)
                         group.modifyTimestamp = new Date(group.modifyTimestamp*1000).toLocaleString()
-                    });
+                    })
+                    let groups = res.data.groups.filter(group => {
+                        if (search && !group.name.toLowerCase().includes(search.toLowerCase())) {
+                            return false
+                        }
+                        return true;
+                    })
+
+                    this.$data.groups = groups;
+                    this.$data.totalGroups = res.data.total
                     this.$data.loading = false
                 })
         },
@@ -161,7 +195,7 @@ export default defineComponent({
                         if (res.data.status == 0) {
                             this.$data.editingGroup.score =  this.$data.editingScore
                             this.$data.editing = false
-                            this.loadItems()
+                            this.loadItems({ page, itemsPerPage, sortBy })
                         }
                     })
             }
